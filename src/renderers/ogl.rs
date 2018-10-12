c183d7069751190e0dedec20a7d088ad4dbeb28e
Petr Volf <Petr.volf216@gmail.com>
2018-10-12 11:14:34 +0200

    massive opengl speedup

--- src/renderers/ogl.rs
+++ src/renderers/ogl.rs
@@ -60,8 +60,43 @@ impl Renderer for OGL {
         let context = glutin::ContextBuilder::new().with_vsync(true).with_multisampling(2);
         let display = Display::new(window, context, &events_loop).unwrap();
 
+        let shape: Vec<Vertex> = OGL::get_hex_points(&map.field[0]);
+        let vertex_buffer = VertexBuffer::new(&display, &shape).unwrap();
+
         implement_vertex!(Vertex, position);
         let indices = index::NoIndices(index::PrimitiveType::TrianglesList);
+
+
+        // get instance parameters
+        let mut per_instance = {
+            #[derive(Copy, Clone)]
+            struct Attr {
+                world_position: (f32, f32),
+                color: (f32, f32, f32)
+            }
+
+            implement_vertex!(Attr, world_position, color);
+
+            let data = map.field.iter().map(|hex| {
+                let color = match hex.terrain_type {
+                    HexType::WATER => (0.29, 0.5, 0.84),
+                    HexType::FIELD => (0.45, 0.75, 0.33),
+                    HexType::ICE => (0.79, 0.82, 0.82),
+                    HexType::MOUNTAIN => (0.3, 0.3, 0.3),
+                    HexType::FOREST => (0.38, 0.6, 0.2),
+                    HexType::OCEAN => (0.23, 0.45, 0.8),
+                    HexType::TUNDRA => (0.3, 0.4, 0.38),
+                    HexType::DESERT => (0.85, 0.84, 0.75),
+                    _ => (0.0, 0.0, 0.0)
+                };
+                Attr {
+                    world_position: (hex.center_x - 0.5, hex.center_y - RATIO / 2.0),
+                    color
+                }
+            }).collect::<Vec<_>>();
+
+            vertex::VertexBuffer::dynamic(&display, &data).unwrap()
+        };
 
         let vertex_shader_src = r#"
             #version 140
@@ -70,21 +105,26 @@ impl Renderer for OGL {
             uniform float total_y;
 
             in vec2 position;
+            in vec2 world_position;
+            in vec3 color;
+
+            out vec3 v_color;
 
             void main() {
-                gl_Position = vec4(position.x/total_x*2 - 1, (-position.y/total_y*2) + 1, 0.0, 1.0);
+                gl_Position = vec4((position.x + world_position.x)/total_x*2 - 1, (-(position.y + world_position.y)/total_y*2) + 1, 0.0, 1.0);
+                v_color = color;
             }
         "#;
 
         let fragment_shader_src = r#"
             #version 140
 
-            uniform vec3 u_color;
+            in vec3 v_color;
 
             out vec4 color;
 
             void main() {
-                color = vec4(u_color, 1.0);
+                color = vec4(pow(v_color.x, 2.2), pow(v_color.y, 2.2), pow(v_color.z, 2.2), 1.0);
             }
         "#;
 
@@ -96,22 +136,7 @@ impl Renderer for OGL {
             if !paused {
                 let mut target = display.draw();
                 target.clear_color(0.0, 0.0, 0.0, 1.0);
-                for hex in &map.field {
-                    let shape: Vec<Vertex> = OGL::get_hex_points(hex);
-                    let vertex_buffer = VertexBuffer::new(&display, &shape).unwrap();
-                    let color: [f32;3] = match hex.terrain_type {
-                        HexType::WATER => [0.29, 0.5, 0.84],
-                        HexType::FIELD => [0.45, 0.75, 0.33],
-                        HexType::ICE => [0.79, 0.82, 0.82],
-                        HexType::MOUNTAIN => [0.3, 0.3, 0.3],
-                        HexType::FOREST => [0.38, 0.6, 0.2],
-                        HexType::OCEAN => [0.23, 0.45, 0.8],
-                        HexType::TUNDRA => [0.3, 0.4, 0.38],
-                        HexType::DESERT => [0.85, 0.84, 0.75],
-                        _ => [0.0, 0.0, 0.0]
-                    };
-                    target.draw(&vertex_buffer, &indices, &program, &uniform! { u_color: color, total_x: map.absolute_size_x, total_y: map.absolute_size_y }, &Default::default()).unwrap();
-                }
+                target.draw((&vertex_buffer, per_instance.per_instance().unwrap()), &indices, &program, &uniform! {total_x: map.absolute_size_x, total_y: map.absolute_size_y }, &Default::default()).unwrap();
                 target.finish().unwrap();
             }
 
