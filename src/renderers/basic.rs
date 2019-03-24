01036edfd586cc984e57b7292d5b9eeb1bdddb02
Petr Volf <Petr.volf216@gmail.com>
2019-03-24 23:51:40 +0100

    fixed software rendering

--- src/renderers/basic.rs
+++ src/renderers/basic.rs
@@ -13,46 +13,57 @@ pub struct Basic {
 }
 
 impl Basic {
-    fn render_polygon (&self, points: &[(u32, u32);6], img: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, color: Rgb<u8>) {
-        let mut min_x = img.width() as i32 - 1;
-        let mut min_y = img.height() as i32 - 1;
-
-        let mut max_x: i32 = 0;
-        let mut max_y: i32 = 0;
-
-        for point in points {
-            min_x = min_x.min(point.0 as i32);
-            min_y = min_y.min(point.1 as i32);
-            max_x = max_x.max(point.0 as i32);
-            max_y = max_y.max(point.1 as i32);
-        };
-
-        max_x = max_x.min(img.width() as i32 - 1);
-        max_y = max_y.min(img.height() as i32 - 1);
-
-        min_x = min_x.max(0);
-        min_y = min_y.max(0);
-
-        let mut deltas: Vec<(i32, i32)> = Vec::with_capacity(points.len());
-        let mut edges: Vec<i32> = Vec::with_capacity(points.len());
-
-        for i in 0..points.len() {
-            deltas.push((points[(i+1)%points.len()].0 as i32 - points[i].0 as i32, points[(i+1)%points.len()].1 as i32 - points[i].1 as i32));
-            edges.push(((min_x - points[i].0 as i32) * deltas[i].1) - ((min_y - points[i].1 as i32) * deltas[i].0));
+    fn render_polygon (&self, points: &[(f32, f32)], img: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, color: Rgb<u8>) {
+        if points.len() < 3 {
+            return;
         }
 
-        for y in min_y..=max_y {
-            let reversed = (y - min_y) % 2 == 0;
-            let x_range: Box<dyn Iterator<Item = i32>> = if reversed {
+        let mut min_x = points[0].0;
+        let mut min_y = points[0].1;
+
+        let mut max_x = points[0].0;
+        let mut max_y = points[0].1;
+
+        for point in &points[1..] {
+            min_x = min_x.min(point.0);
+            min_y = min_y.min(point.1);
+            max_x = max_x.max(point.0);
+            max_y = max_y.max(point.1);
+        };
+
+        max_x = max_x.min(img.width() as f32);
+        max_y = max_y.min(img.height() as f32);
+
+        min_x = min_x.max(0.0);
+        min_y = min_y.max(0.0);
+
+
+        // properly round float coordinates 
+        let min_x = min_x.max(0.0).min(img.width() as f32 - 1.0).round() as i32;
+        let min_y = min_y.max(0.0).min(img.height() as f32 - 1.0).round() as i32;
+        let max_x = max_x.max(0.0).min(img.width() as f32 - 1.0).round() as i32;
+        let max_y = max_y.max(0.0).min(img.height() as f32 - 1.0).round() as i32;
+
+        let mut deltas: Vec<(f32, f32)> = Vec::with_capacity(points.len());
+        let mut edges: Vec<f32> = Vec::with_capacity(points.len());
+
+        for i in 0..points.len() {
+            deltas.push((points[(i + 1) % points.len()].0 - points[i].0, points[(i + 1) % points.len()].1 - points[i].1));
+            edges.push(((min_x as f32 + 0.5 - points[i].0) * deltas[i].1) - ((min_y as f32 + 0.5 - points[i].1) * deltas[i].0));
+        }
+
+        for y in (min_y)..=(max_y) {
+            let is_reversed = ((y - min_y) % 2) != 0;
+            let x_range: Box<dyn Iterator<Item = i32>> = if is_reversed {
                 Box::new((min_x..=max_x).rev())
             } else {
                 Box::new(min_x..=max_x)
             };
 
-            for x in x_range {
+            for (x_index, x) in x_range.enumerate() {
                 let mut in_triangle = true;
                 for edge in &edges {
-                    if *edge < 0 {
+                    if *edge < 0.0 {
                         in_triangle = false;
                         break;
                     }
@@ -61,11 +72,14 @@ impl Basic {
                     img.put_pixel(x as u32, y as u32, color);
                 }
 
-                for (index, edge) in edges.iter_mut().enumerate() {
-                    if reversed {
-                        *edge += deltas[index].1;
-                    } else {
-                        *edge -= deltas[index].1;
+                // dont add offset if the tested pixel is last on line
+                if x_index as i32 != (min_x - max_x).abs() {
+                    for (index, edge) in edges.iter_mut().enumerate() {
+                        if is_reversed {
+                            *edge -= deltas[index].1;
+                        } else {
+                            *edge += deltas[index].1;
+                        }
                     }
                 }
             }
@@ -83,10 +97,10 @@ impl Basic {
 
         // get hex vertices positions
         // points need to be in counter clockwise order
-        let mut points = [(0,0);6];
-        for index in (0..6).rev() {
+        let mut points = [(0.0, 0.0);6];
+        for index in 0..6 {
             let coords = Basic::get_hex_vertex(hex, index);
-            points[index] = ((coords.0 * self.multiplier) as u32, (coords.1 * self.multiplier) as u32);
+            points[5 - index] = (coords.0 * self.multiplier, coords.1 * self.multiplier);
         };
 
         let mut color = match hex.terrain_type {
@@ -102,7 +116,7 @@ impl Basic {
             _ => Rgb([0, 0, 0])
         };
         for i in 0..3 {
-            color.data[i] = (color.data[i] as f32 * color_diff) as u8;
+            color.data[i] = (f32::from(color.data[i]) * color_diff) as u8;
         }
 
         self.render_polygon(&points, img, color);
@@ -125,6 +139,8 @@ impl Renderer for Basic {
         for hex in &map.field {
             self.render_hex(&mut imgbuf, hex);
         }
+        // Test polygon
+        //self.render_polygon(&[(10.0, 10.0),(20.0, 400.0),(400.0, 10.0)], &mut imgbuf, Rgb([128,128,128]));
         imgbuf
     }
 
